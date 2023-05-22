ea8a33ad01c713444917c477882d604922ddc9e3
okrajh <himanshuraj1209@gmail.com>
2023-05-23 00:51:38 +0530

    In React.js 18.0 We create React Component without importing react library

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,24 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-function Container(props){
-    return(
-        <div>
-            <h1>Hell'o {props.name}</h1>
-            <Box name="Radhe"></Box>
-        </div>
+import App from "./App";
 
-    )
-}
-
-function Box(props){
-    return(
-        <div>
-            <h1>Box {props.name}</h1>
-        </div> 
-    )
-
-}
-
-const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(<Container name="Himanshu Raj"/>)
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
