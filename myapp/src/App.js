3ceb703671abdae8eb1c69831f1c14a142d37864
okrajh <himanshuraj1209@gmail.com>
2023-06-29 08:27:43 +0530

    ch:02=>Create an jsx element inside App.js file and export(export default jsx_element) those and import (import {element} from './App.js') into index.js file and render that using (ReactDOM.render(element,root_node)

---
+++ myapp/src/App.js
@@ -0,0 +1,4 @@
+import React from "react";
+var name = "Learner";
+var element = <h1>Hello, {name}.Welcome to GeeksforGeeks.</h1>;
+export default element
---
+++ myapp/src/index.js
@@ -0,0 +1,3 @@
+import ReactDOM from "react-dom";
+import element from "./App";  
+ReactDOM.render(element, document.getElementById("root"));
